307ea1d0082334126c7350cc66e01c87d5504f7a
Michael Thomsen <nesmomik@gmail.com>
2025-11-14 16:13:59 +0100

    Added missing display of filter option, Created appropriate main function

--- filter_users.py
+++ filter_users.py
@@ -3,9 +3,9 @@ import json
 def filter_users_by_name(name):
     with open("users.json", "r") as file:
         users = json.load(file)
-    
+
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
-    
+
     for user in filtered_users:
         print(user)
 
@@ -13,7 +13,7 @@ def filter_users_by_name(name):
 def filter_users_by_age(age):
     with open("users.json", "r") as file:
         users = json.load(file)
-    
+
     filtered_users = [user for user in users if user["age"] == int(age)]
 
     for user in filtered_users:
@@ -23,16 +23,17 @@ def filter_users_by_age(age):
 def filter_users_by_email(email):
     with open("users.json", "r") as file:
         users = json.load(file)
-    
+
     filtered_users = [user for user in users if user["email"] == email]
 
     for user in filtered_users:
         print(user)
 
+def main(): 
+    filter_option = (
+        input("What would you like to filter by? ('name', 'age', 'email'): ").strip().lower()
+    )
 
-if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? ('name' or 'age'): ").strip().lower()
-    
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
@@ -44,3 +45,7 @@ if __name__ == "__main__":
         filter_users_by_email(email_to_search)
     else:
         print("Filtering by that option is not yet supported.")
+
+
+if __name__ == "__main__":
+    main()
